ca7f8daef5237551fb71e9389f9bf4467dc93f19
Mayura Pathirana <mayurapathirana@ymail.com>
2023-05-14 11:36:17 +0530

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,15 @@
+#decorators used to increase a functanality of a excicting function
+def start_end_print(func):          #decorator function takes the function need to be changed as argument
+    def wrapper():                  #wrapper will take the all functanality together and will return
+        print("Starts")             #things we vcan add before the function
+        func()                      #function need to be changed
+        print("Ends")               #things we can add after the function
+    return wrapper                  #returning the wrapper
+
+@start_end_print                                #linking decorator to the function
+def print_name():                               #function needed to be changed
+    print("Mayura")
+
+#print_name = start_end_print(print_name)        #assinging the decorator to function(we dont need this line if we add @stsrt_end_print above the function)
+
+print_name()
